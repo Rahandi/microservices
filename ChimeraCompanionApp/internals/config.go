012515b0edea2226b62fed9316878a72ad4070a5
Rahandi Noor Pasha <rahandi.pasha@traveloka.com>
2023-11-11 09:10:25 +0700

    Add ChimeraCompanionApp with config and env files

---
+++ ChimeraCompanionApp/internals/config.go
@@ -0,0 +1,51 @@
+package internals
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Token string `mapstructure:"TOKEN"`
+
+	Admins []int64 `mapstructure:"ADMINS"`
+}
+
+func NewConfig() *Config {
+	config := Config{}
+
+	file, err := os.Open("config.json")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			panic(err)
+		}
+	} else {
+		defer file.Close()
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&config)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	// Bind each key in the Config struct to a corresponding environment variable
+	keys := []string{
+		"TOKEN",
+	}
+	for _, key := range keys {
+		viper.BindEnv(key)
+	}
+
+	_ = viper.ReadInConfig() // Ignore error if .env file does not exist
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	return &config
+}
---
+++ ChimeraCompanionApp/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"ChimeraCompanionApp/internals"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	config := internals.NewConfig()
+	bot, err := tgbotapi.NewBotAPI(config.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
